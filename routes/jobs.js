a722d380489471c42eb540b76afae4ef8a0554d3
EnzoFonseca04 <fonseca.enzo06@gmail.com>
2026-02-23 14:32:24 -0300

    Adicionando Rota e Insert - Ver0.4

---
+++ routes/jobs.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router  = express.Router();
+const Job     = require('../models/Job');
+
+router.get('/test', (req, res) => {
+    res.send('Teste de rota');
+});
+
+// add job via post
+router.post('/add', (req, res) => {
+
+    let { title, descripition, salary, company, email, new_job } = req.body;
+
+
+    //insert
+    Job.create({
+        title,
+        descripition,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+});
+
+module.exports = router;
